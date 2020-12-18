15f7c0b3ed323577c2017243c4174fbfc84098b7
Aditek <aditek1105@gmail.com>
2020-12-18 15:15:13 -0500

    update

---
+++ DYNAMIC-OBJECT-KEYS/index.js
@@ -0,0 +1,11 @@
+const { ConsoleErrorListener } = require("antlr4/error/ErrorListener")
+
+// the dot notation of accessing the attributes within an object
+const person = {
+    name: 'Aditya'
+}
+console.log(person.name)
+person.age = 23
+console.log(person.age)
+
+// using square bracket notation
