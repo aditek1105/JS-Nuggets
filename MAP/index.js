fe9e6b665764ad7317e4510bc7e3620401ae52a2
Aditek <aditek1105@gmail.com>
2020-12-18 13:28:00 -0500

    Map updated

---
+++ MAP/index.js
@@ -0,0 +1,16 @@
+// use of MAP in javascript
+const obj1 = {
+    name: 'Aditya',
+    age: 23,
+    location: 'New Delhi'
+}
+const obj2 = {
+    name: 'Aditya Tekur',
+    age: 23,
+    location: 'Chennai'
+}
+const obj3 = {
+    name: 'Aditek',
+    age: 21,
+    location: 'Boston'
+}
