6d95ca6d504171907fd163cb82f57948fe10fe97
Aditek <aditek1105@gmail.com>
2020-12-18 13:51:09 -0500

    update

--- MAP/index.js
+++ MAP/index.js
@@ -2,18 +2,16 @@
 /**
  * About MAP, it returns a new array, does not change the size of the original array. 
  */
-const obj1 = {
+const obj1 = [{
     name: 'Aditya',
     age: 23,
     location: 'New Delhi'
-}
-const obj2 = {
+},{
     name: 'Aditya Tekur',
     age: 23,
     location: 'Chennai'
-}
-const obj3 = {
+},{
     name: 'Aditek',
     age: 21,
     location: 'Boston'
-}
+}];
