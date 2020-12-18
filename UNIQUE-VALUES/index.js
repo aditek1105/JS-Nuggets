772da9c0efb37c04b582f3f063e7a64676bd6dec
Aditek <aditek1105@gmail.com>
2020-12-18 14:54:14 -0500

    update

---
+++ UNIQUE-VALUES/index.js
@@ -0,0 +1,34 @@
+//To get the unique values from the array.
+
+const menu = [{
+name: 'pancakes',
+category: 'Breakfast'
+},{
+name: 'burger',
+category: 'Lunch'
+}, {
+    name: 'dal makhani',
+    category: 'Lunch'
+}, {
+    name: 'Kadai Paneer',
+    category: 'Dinner'
+}, {
+    name: 'Pizza',
+    category: 'Dinner'
+}, {
+    name: 'idli',
+    category: 'Breakfast'
+}]
+
+var categories = menu.map(item => item.category)
+console.log(categories)
+
+// to get the unique values from the array, we use set
+categories = new Set(menu.map(item => item.category))
+console.log(categories)
+// Now categories is a set which is a object, to convert it back to array, we use spread operator
+categories = [...new Set(menu.map(item => `<h2>${item.category}</h2>`))]
+console.log(categories)
+
+const display = document.querySelector("#result");
+display.innerHTML = categories.join('')
