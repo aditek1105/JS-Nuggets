b836656766d3a9335dc70ca98fbd9f967d77235b
Aditek <aditek1105@gmail.com>
2020-12-19 15:25:47 -0500

    update

--- REST/index.js
+++ REST/index.js
@@ -14,5 +14,19 @@ console.log(specificFruit)
 
 // objects
 const person = { name: 'Aditya', lastName: 'Tekur', job: 'Developer'}
-const {name,...restData} = person
-console.log(name,restData)
+const {job,...restData} = person
+console.log(job,restData)
+
+//functions
+
+const getAverage = (name,...scores) => {
+console.log(name)
+console.log(scores)
+const average = scores.reduce((total,item)=>{
+    console.log(total)
+    return total += item
+},0)/scores.length
+console.log(average)
+}
+
+getAverage(person.name,10,10,10)
